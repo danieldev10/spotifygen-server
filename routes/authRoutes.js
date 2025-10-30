610f985afae6f5500be4871b5387c1f86c81599e
Achonma Daniel Tanko <danielachonma10@gmail.com>
2025-10-30 23:03:40 +0100

    testing new routing method

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -185,7 +185,11 @@ router.get('/spotify', passport.authenticate('spotify', {
 
 router.get('/spotify/callback', passport.authenticate('spotify', { session: false }), (req, res) => {
     const jwtToken = jwt.sign({ id: req.user.id }, process.env.JWT_KEY, { expiresIn: '3h' });
-    res.redirect(`${process.env.FRONTEND_URL}/oauth-success?token=${jwtToken}`);
+    // res.redirect(`${process.env.FRONTEND_URL}/oauth-success?token=${jwtToken}`);
+    if (jwtToken) {
+        localStorage.setItem('token', jwtToken);
+        res.redirect(`${process.env.FRONTEND_URL}/`)
+    }
 });
 
 export default router;
